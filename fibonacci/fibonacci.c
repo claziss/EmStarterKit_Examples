6633ee291bee2d5ada50bea93853ad0f3c736365
Claudiu Zissulescu <claziss@synopsys.com>
2013-11-26 10:09:53 +0100

    Remove proprietary Synopsys copyrights

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -3,22 +3,6 @@
 /**                                                                     **/
 /** Copyright (C) 1989-2013 Synopsys, Inc.                              **/
 /** All Rights Reserved.                                                **/
-/**                                                                     **/
-/** SYNOPSYS CONFIDENTIAL                                               **/
-/**                                                                     **/
-/** This is an unpublished proprietary work of Synopsys, Inc., and is   **/
-/** fully protected under copyright and trade secret laws. You may not  **/
-/** view, use, disclose, copy, or distribute this file or any           **/
-/** information contained herein except pursuant to a valid written     **/
-/** license from Synopsys.                                              **/
-/**                                                                     **/
-/** For more information, contact                                       **/
-/** est-adm@synopsys.com or call (650) 584-1631                         **/
-/**                                                                     **/
-/*************************************************************************/
-/*************************************************************************/
-
-
 /***************************************************************/
 //	LOCAL DEFINES
 /***************************************************************/
